c7e800023280d942b8bb1dddc4fcdac348029071
LukasBluebaum <lukasbluebaumb94@gmail.com>
2024-12-04 09:12:20 +0100

    update day4

--- day4/2.py
+++ day4/2.py
@@ -7,9 +7,9 @@ def check(i, j, a, b, c, d):
          lines[i-1][j+1] == c and lines[i+1][j+1] == d
 
 res = 0
-for i in range(len(lines)):
-  for j in range(len(lines[0])):
-    if lines[i][j] == 'A' and i > 0 and j > 0 and i < len(lines) - 1 and j < len(lines[0]) - 1:
+for i in range(1, len(lines)-1):
+  for j in range(1, len(lines[0])-1):
+    if lines[i][j] == 'A':
       res += (check(i, j, 'M', 'M', 'S', 'S') or check(i, j, 'S', 'S', 'M', 'M') or
               check(i, j, 'M', 'S', 'M', 'S') or check(i, j, 'S', 'M', 'S', 'M'))
 
